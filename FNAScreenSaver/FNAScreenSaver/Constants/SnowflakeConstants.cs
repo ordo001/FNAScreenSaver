f9d1f9189e887be1694a12b18011daee68f5013d
ordo001 <113034616+ordo001@users.noreply.github.com>
2025-11-02 16:09:09 +0300

    гоу

---
+++ FNAScreenSaver/FNAScreenSaver/Constants/SnowflakeConstants.cs
@@ -0,0 +1,54 @@
+﻿namespace FNAScreenSaver.Constants
+{
+    /// <summary>
+    /// Класс констант для снежинки
+    /// </summary>
+    public class SnowflakeConstants
+    {
+        /// <summary>
+        /// Количество снежинок
+        /// </summary>
+        public const int Count = 1000;
+
+        /// <summary>
+        /// Минимальный размер снежинки
+        /// </summary>
+        public const float MinScale = 0.03f;
+
+        /// <summary>
+        /// Максимальный размер снежинки
+        /// </summary>
+        public const float MaxScale = 0.1f;
+
+        /// <summary>
+        /// Минимальная скорость
+        /// </summary>
+        public const float MinSpeed = 60f;
+
+        /// <summary>
+        /// Максимальная скорость
+        /// </summary>
+        public const float MaxSpeed = 180f;
+
+        /// <summary>
+        /// Минимальная точка появления
+        /// </summary>
+        public const int MinStartY = 50;
+
+        /// <summary>
+        /// Максимальная точка появления
+        /// </summary>
+        public const int MaxStartY = 1000;
+
+        /// <summary>
+        /// Минимальное значение прозрачности
+        /// </summary>
+        public const float MinAlpha = 0.5f;
+
+        /// <summary>
+        /// Максимальное значение прозрачности
+        /// </summary>
+        public const float MaxAlpha = 1.0f;
+
+    }
+}
---
+++ FNAScreenSaver/FNAScreenSaver/Services/GameEngine.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FNAScreenSaver.Constants;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Color = Microsoft.Xna.Framework.Color;
+
+namespace FNAScreenSaver.Services
+{
+    /// <summary>
+    /// Основной класс игры
+    /// </summary>
+    public class GameEngine : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private Texture2D backgroundTexture;
+        private Texture2D snowflakeTexture;
+
+        private SnowflakeManager snowflakeManager;
+
+        /// <summary>
+        /// Инициализировать новый экземпляр <see cref="GameEngine"/>
+        /// </summary>
+        public GameEngine()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.IsFullScreen = true;
+            graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+            IsMouseVisible = false;
+        }
+
+        protected override void Initialize()
+        {
+            var screenWidth = graphics.PreferredBackBufferWidth;
+            var screenHeight = graphics.PreferredBackBufferHeight;
+
+            snowflakeManager = new SnowflakeManager(SnowflakeConstants.Count, screenWidth, screenHeight);
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            backgroundTexture = Content.Load<Texture2D>("forest");
+            snowflakeTexture = Content.Load<Texture2D>("snowflake");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().GetPressedKeys().Length > 0)
+            {
+                Exit();
+            }
+            snowflakeManager.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(backgroundTexture, GraphicsDevice.Viewport.Bounds, Color.White);
+
+            var snowflakes = snowflakeManager.GetAllSnowflakes();
+            foreach (var snowflake in snowflakes)
+            {
+                var origin = new Vector2(snowflakeTexture.Width / 2, snowflakeTexture.Height / 2);
+                var position = new Vector2(snowflake.X, snowflake.Y);
+
+                spriteBatch.Draw(
+                    snowflakeTexture,
+                    position,
+                    null,
+                    snowflake.Color,
+                    0,
+                    origin,
+                    snowflake.Scale,
+                    SpriteEffects.None,
+                    0f
+                );
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        protected override void UnloadContent()
+        {
+            spriteBatch?.Dispose();
+            backgroundTexture?.Dispose();
+            snowflakeTexture?.Dispose();
+
+            base.UnloadContent();
+        }
+
+    }
+}
